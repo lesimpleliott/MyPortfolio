d68a470d15858a5a7ede3c687a39bc1155302280
Eliott Lesimple <lesimpleliott@gmail.com>
2024-05-03 00:06:26 +0200

    add stickyButton in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "./Logo";
+import StickyButton from "./StickyButton";
 
 const Header = () => {
+  const [theme, setTheme] = useState("dark");
+
   const toggleTheme = () => {
-    const theme = document.body.getAttribute("data-theme");
+    // const theme = document.body.getAttribute("data-theme");
     if (theme === "dark") {
       document.body.setAttribute("data-theme", "light");
+      setTheme("light");
     } else {
       document.body.setAttribute("data-theme", "dark");
+      setTheme("dark");
     }
   };
 
@@ -16,40 +22,48 @@ const Header = () => {
     <HeaderStyled>
       <div className="container">
         <NavLink to="/">
-          <Logo height={40} />
+          <Logo height={30} />
         </NavLink>
 
         <div className="navbar">
           <ul className="navbar">
-            <li>
-              <NavLink to="#stack">Stack</NavLink>
-            </li>
-            <li>
-              <a href="#projects">Projets</a>
-            </li>
-            <li>
-              <a href="/about">À propos</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
+            <NavLink to="/#stack">
+              <li>Stack</li>
+            </NavLink>
+            <NavLink to="/#projects">
+              <li>Projets</li>
+            </NavLink>
+            <NavLink to="/about">
+              <li>À propos</li>
+            </NavLink>
+            <NavLink to="/contact">
+              <li>Contact</li>
+            </NavLink>
           </ul>
         </div>
 
         <section className="icons">
-          <button className="headerBtn" id="toggleThemeBtn">
-            <img src="./icons/github_white.webp" alt="" />
-          </button>
-          <button className="headerBtn" id="toggleThemeBtn">
-            <img src="./icons/linkedin_white.webp" alt="" />
-          </button>
-          <button
-            onClick={toggleTheme}
-            className="headerBtn"
-            id="toggleThemeBtn"
-          >
-            <img src="./icons/theme_white.webp" alt="" />
-          </button>
+          <StickyButton
+            link="https://github.com/lesimpleliott"
+            theme={theme}
+            icon="./icons/github_white.webp"
+            iconLight="./icons/github_black.webp"
+            alt="github icon"
+          />
+          <StickyButton
+            link="https://www.linkedin.com/in/lesimpleliott/"
+            theme={theme}
+            icon="./icons/linkedin_white.webp"
+            iconLight="./icons/linkedin_black.webp"
+            alt="linkedin icon"
+          />
+          <StickyButton
+            function={toggleTheme}
+            theme={theme}
+            icon="./icons/theme_white.webp"
+            iconLight="./icons/theme_black.webp"
+            alt="theme icon"
+          />
         </section>
       </div>
     </HeaderStyled>
@@ -74,30 +88,24 @@ const HeaderStyled = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    gap: 30px;
+    gap: 20px;
   }
 
   .navbar {
     flex: 1;
     display: flex;
     justify-content: end;
-    gap: 20px;
-
+    gap: 15px;
     font-size: 1rem;
+    font-weight: 500;
+    li {
+      cursor: pointer;
+    }
   }
 
   .icons {
     display: flex;
-    gap: 15px;
-
-    .headerBtn {
-      all: unset;
-
-      img {
-        height: 25px;
-        width: 25px;
-      }
-    }
+    align-items: center;
   }
 `;
 
---
+++ src/components/StickyButton.tsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+type StickyBtnProps = {
+  function?: () => void;
+  link?: string;
+  theme: string;
+  icon: string;
+  iconLight: string;
+  alt: string;
+};
+
+const StickyButton: React.FC<StickyBtnProps> = (props) => {
+  const anim = () => {
+    const btns = document.querySelectorAll(".btn");
+
+    btns.forEach((btn) => {
+      btn.addEventListener("mouseover", (e) => {
+        const mouseEvent = e as MouseEvent;
+        (btn as HTMLElement).style.transform = `translate(${
+          mouseEvent.offsetX - 20
+        }px, ${mouseEvent.offsetY - 13}px)`;
+      });
+      btn.addEventListener("mouseleave", () => {
+        (btn as HTMLElement).style.transform = `translate(0px, 0px)`;
+      });
+    });
+  };
+
+  return (
+    <StickyButtonStyled>
+      {/* Logique si props.function est défini */}
+      {props.function && (
+        <button onMouseOver={anim} className="btn" onClick={props.function}>
+          <img
+            src={props.theme === "dark" ? props.icon : props.iconLight}
+            alt={props.alt}
+          />
+        </button>
+      )}
+      {/* Logique si props.link est défini */}
+      {props.link && (
+        <a onMouseOver={anim} className="btn" href={props.link} target="_blank">
+          <img
+            src={props.theme === "dark" ? props.icon : props.iconLight}
+            alt={props.alt}
+          />
+        </a>
+      )}
+    </StickyButtonStyled>
+  );
+};
+
+const StickyButtonStyled = styled.div`
+  width: 40px;
+  height: 50px;
+  display: flex;
+
+  .btn {
+    all: unset;
+    cursor: pointer;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: transform 1000ms ease-out;
+
+    img {
+      width: 25px;
+      height: 25px;
+    }
+  }
+`;
+
+export default StickyButton;
